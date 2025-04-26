4d1c3cadd312c017fa378c4c703048088c12e1a9
Bianca <biancalexandra.2003@gmail.com>
2025-04-26 12:22:09 +0300

    Faza 5- Fixare erori pentru asocierea unui subtask/comentariu cu task

--- src/main/java/com/example/task_management/service/CommentService.java
+++ src/main/java/com/example/task_management/service/CommentService.java
@@ -1,7 +1,9 @@
 package com.example.task_management.service;
 
 import com.example.task_management.model.Comment;
+import com.example.task_management.model.Task;
 import com.example.task_management.repository.CommentRepository;
+import com.example.task_management.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,23 @@ import java.util.List;
 public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
+    @Autowired
+    private TaskRepository taskRepository;
 
     //Metoda pentru adaugarea unui comentariu la un task
-    public Comment addComment(Comment comment){
-        return commentRepository.save(comment);
+    public Comment addComment(Comment comment) {
+
+        Long taskId = comment.getTask().getId();
+        Task task = taskRepository.findById(taskId).orElse(null);
+
+        if (task != null) {
+            comment.setTask(task);
+            return commentRepository.save(comment);
+        } else {
+            return null;
+        }
     }
+
 
     //Metoda pentru afisarea tuturor comentariilor pentru un task
     public List<Comment> getCommentForTask(Long taskId){
--- src/main/java/com/example/task_management/service/SubtaskService.java
+++ src/main/java/com/example/task_management/service/SubtaskService.java
@@ -1,7 +1,9 @@
 package com.example.task_management.service;
 
 import com.example.task_management.model.Subtask;
+import com.example.task_management.model.Task;
 import com.example.task_management.repository.SubtaskRepository;
+import com.example.task_management.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,20 @@ import org.springframework.stereotype.Service;
 public class SubtaskService {
     @Autowired
     private SubtaskRepository subtaskRepository;
+    @Autowired
+    private TaskRepository taskRepository;
 
     //Metoda pentru adaugarea unui subtask
     public Subtask addSubtask(Subtask subtask) {
-        return subtaskRepository.save(subtask);
+
+        Long taskId = subtask.getTask().getId();
+        Task task=taskRepository.findById(taskId).orElse(null);
+
+        if(task!=null){
+            subtask.setTask(task);
+            return subtaskRepository.save(subtask);
+        }else {
+            return null;
+        }
     }
 }
